27d7f279bb7a6545a376ebf6ea83a82345cb3e5f
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-11-22 14:35:35 +0900

    이렇게 해야 되려나?

--- temp/week11/Program_Counter.v
+++ temp/week11/Program_Counter.v
@@ -7,18 +7,32 @@ output [3:0] PC;
 // write your code for reading/writing
 reg [3:0] PC;
 initial begin 
-	PC = 4'b0000; 
+	PC = 4'b0000;
 end
 always @(posedge CLK) begin
+	// if(PL==0) PC = PC+1;
+	// else if(JB) PC = AData;
+	// if(BC) begin
+	// 	if(AData[3]) PC = PC+{LAddress,RAddress};
+	// 	else PC = PC+1;
+	// end
+	// if(BC==0) begin
+	// 	if(AData == 4'b0000) PC = PC+{LAddress,RAddress};
+	// 	else PC = PC+1;
+	// end
 	if(PL==0) PC = PC+1;
-	else if(JB) PC = AData;
-	if(BC) begin
-		if(AData[3]) PC = PC+{LAddress,RAddress};
-		else PC = PC+1;
-	end
-	if(BC==0) begin
-		if(AData == 4'b0000) PC = PC+{LAddress,RAddress};
-		else PC = PC+1;
+	else begin
+		if(JB) PC = AData;
+		else begin
+			if(BC) begin
+				if(AData<0) PC = PC+{LAddress,RAddress};
+				else PC = PC+1;
+			end
+			else begin
+				if(AData == 4'b0000) PC = PC+{LAddress,RAddress};
+				else PC = PC+1;
+			end
+		end
 	end
 end
 endmodule
